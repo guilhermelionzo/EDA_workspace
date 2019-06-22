0ddc614e9209a2e10b31aa75b4f92b18c3cbbe7b
Guilherme Lionço <guilherme.lionzo@gmail.com>
2019-06-22 11:49:19 -0300

    Create aula_22.6.c

---
+++ aulas/aula_22.6.c
@@ -0,0 +1,17 @@
+/*Ideias para o trabalho 2:*/
+
+/*
+1. As variaveis da expressao infixa aparecem na mesma ordem na pos-fixa.
+2. Os operadores devem ser empilhados em ordem de prioridade: so empilhar se a prioridade do operador
+sendo lido for maior que aquele que esta no topo da pilha.
+3.Prioridade dos operadores sendo lidos:
+  0_ (
+  1_ ^
+  2_ /or*
+  3_ +or-
+4.prioridade na pilha
+  0_ ^
+  1_ /or*
+  2_ +or-
+  3_ (
+  */
